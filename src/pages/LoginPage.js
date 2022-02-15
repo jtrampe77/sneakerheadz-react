5bab7c8a9dd0742137566b9cd86a82f1fece0d2d
John Trampe <73605029+jtrampe77@users.noreply.github.com>
2022-02-15 13:37:53 -0700

    change

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import {AppBar} from './../components/appbar'
-import {FootBar} from './../components/footbar'
-import {HeroBanner} from './../components/herobanner'
+
 
 
 function LoginPage(props) {
