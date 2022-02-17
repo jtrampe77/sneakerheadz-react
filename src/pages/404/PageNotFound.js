ff8697fed808697ffc6d8bf83ca92053b17be557
John Trampe <73605029+jtrampe77@users.noreply.github.com>
2022-02-17 11:15:40 -0700

    404 page

--- src/pages/404/PageNotFound.js
+++ src/pages/404/PageNotFound.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import {PageNotFoundContainer,PageHeader,PageTagline} from './styles'
+import { Link } from 'react-router-dom'
+import Image from '../../img/pagenotfoundimg.jpg'
 
 
 function PageNotFound() {
   return (
     <>
-    <h1>Page Not Found</h1>
+    <PageNotFoundContainer>
+      <div>
+          <PageHeader>SORRY</PageHeader>
+          <PageTagline>Page Not Found</PageTagline>
+      </div>
+    
+    </PageNotFoundContainer>
+ 
     </>
   )
 }
---
+++ src/pages/404/styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const PageNotFoundContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height:80vh;
+`;
+
+const PageHeader = styled.h1`
+  font-size: 15rem;
+  text-align: center;
+  color: red;
+  font-weight:bold;
+`;
+
+const PageTagline = styled.h2`
+    font-size:3rem;
+    text-align: center;
+`;
+
+
+
+export {PageNotFoundContainer,PageHeader,PageTagline}
